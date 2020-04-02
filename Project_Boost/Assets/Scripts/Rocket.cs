5de7ee4082c8de104ad0db35d6c8c867519f5547
ikostov91 <i.kostov91@gmail.com>
2020-04-03 01:05:28 +0300

    ProjectBoost WIP

---
+++ Project_Boost/Assets/Scripts/Rocket.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Parameters
+    [SerializeField] private float _thrustPower = 1f;
+    [SerializeField] private float _rotationSpeed = 1f;
+
+    // Components
+    private Rigidbody _myRigidBody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        this._myRigidBody = GetComponent<Rigidbody>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        // GetKey - continious press
+        // GetKeyDown - one time press
+        if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
+        {
+            this._myRigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("rotate to the left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("rotate to the right");
+        }
+    }
+}
